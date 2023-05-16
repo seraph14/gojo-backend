3046dea9fc31a9f203863137def5383282fff931
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2023-05-16 13:50:21 +0300

    [ref] refactored code comment

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,1 +1,6 @@
 from backend.wsgi import application
+
+'''
+This is the file that is responsible for telling wsgi in shared hosting to use the application
+callable in backend.wsgi to run the production server.
+'''
