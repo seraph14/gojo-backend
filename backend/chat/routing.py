371bdbc5e36e3b64969c8e115f6b00e84f60e534
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2023-05-20 11:48:55 +0300

    [routing] update

--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -5,5 +5,6 @@ from django.urls import re_path
 from chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<username>[\w.@+-]+)/', ChatConsumer.as_asgi())
+    # re_path(r'ws/chat/', ChatConsumer.as_asgi()),
 ]
