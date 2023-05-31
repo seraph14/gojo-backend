d8711592e1317c8fc362295de3402865939cf1d4
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2023-06-01 02:59:40 +0300

    a

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,6 +123,7 @@ DATABASES = {
         'NAME': env("DATABASE_NAME"),
         'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': env("DATABASE_HOST"),
         'PORT' : env("DATABASE_PORT")
     }
 }
